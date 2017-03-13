64caf67231301f69056358de0cedd1b7a1489239
Isaac Mungai <isaacm@users.noreply.github.com>
2017-03-13 10:29:41 -0400

    Skeleton for unit tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,13 @@
 import unittest
 
+from pytransloadit import client
+
 
 class TestClient(unittest.TestCase):
 
-    def test_cli_count(self):
-        assert True
+    def setUp(self):
+        super(TestClient, self).setUp()
+
+    def test_client(self):
+        api_client_obj = client.TransloadItClient('KEY', 'SECRET')
+        self.assertIsNotNone(api_client_obj)
